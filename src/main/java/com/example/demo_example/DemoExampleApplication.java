9497ee2f1b09ddd4920786912c778b7567ff6061
LeeJaeWook <ljw842177@gmail.com>
2018-07-19 09:24:38 +0900

    Initial commit

---
+++ src/main/java/com/example/demo_example/DemoExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExampleApplication.class, args);
+    }
+}
